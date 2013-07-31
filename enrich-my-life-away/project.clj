3dd59ab98ce13a82899cbc02e06320b4caa1dfb6
unknown <pdoctor@dhill-x220i.hq.opinionlab.com>
2013-07-31 17:16:33 -0500

    I can pull from the reddit api but I seem to be misusing before/after api parameters

--- enrich-my-life-away/project.clj
+++ enrich-my-life-away/project.clj
@@ -1,4 +1,4 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] [clj-http "0.7.6"] [org.clojure/data.json "0.2.2"]]
   :main app.core)
--- enrich-my-life-away/src/app/core.clj
+++ enrich-my-life-away/src/app/core.clj
@@ -1,6 +1,48 @@
-(ns app.core)
+(ns app.core
+  (:require [clj-http.client :as client] [clojure.data.json :as json]))
+
+(defn get-reddit-url
+  "Gets new posts from a given subreddit with an optional after parameter"
+  ([subreddit]
+    (clojure.string/join ["http://www.reddit.com/r/" subreddit "/new.json?limit=1"]))
+
+  ([subreddit before]
+    (clojure.string/join [(get-reddit-url subreddit) "?before=" before]))
+)
+
+(defn get-data-from-reddit-api
+  "Gets new posts from a given subreddit"
+  [url]
+  (println url)
+  (:data
+    (json/read-str
+      (:body (client/get url)) :key-fn keyword))
+)
+
+(defn pause-for-reddit-api
+  []
+  (Thread/sleep 5000))
+
+(defn get-next-reddit-post
+  ([before]
+    (pause-for-reddit-api)
+    (if (nil? before)
+      (get-data-from-reddit-api (get-reddit-url "clojure" ))
+      (get-data-from-reddit-api (get-reddit-url "clojure" before))
+  ))
+)
+
+(defn test-loop
+  []
+  (loop [result (get-next-reddit-post nil)]
+    (println (-> result :children first :data :title))
+    (println (-> result :children first :data :name))
+    (recur (get-next-reddit-post (-> result :children first :data :name)))))
+
+
 (defn -main
- "I don't do a whole lot"
- []
- (println "Hello World!"))
+  "I don't do a whole lot."
+  []
+  (test-loop)
+)
 
